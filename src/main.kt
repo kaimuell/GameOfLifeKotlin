7c90e93496e401ab20589e925216cdaad8a367ab
kaimuell <info@kaimueller-art.de>
2024-02-15 12:52:27 +0100

    initialCommit

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,4 @@
+import org.testng.annotations.Test
 import kotlin.random.Random
 
 fun main(){
@@ -44,6 +45,8 @@ fun checkNeighbours(world: Array<Array<Boolean>>, i: Int, j: Int): Int {
     return aliveNeighbours
 }
 
+
+
 private fun isInbounds(i: Int, world: Array<Array<Boolean>>, j: Int) =
     0 <= i && i < world.size && 0 <= j && j < world[i].size
 
@@ -61,4 +64,48 @@ fun generateWorld(height: Int, width: Int): Array<Array<Boolean>> {
     return Array (height) {Array (width){
         Random.nextBoolean()
     } }
+}
+
+
+class TestClass {
+    @Test
+    fun generateWorldTest() {
+        val world = generateWorld(20, 40)
+        assert(world.size == 20)
+        assert(world[0].size == 40)
+    }
+
+    @Test
+    fun isInBoundsTest() {
+        val world = generateWorld(20, 40)
+        assert(isInbounds(0, world, 0))
+        assert(isInbounds(19, world, 39))
+        assert(!isInbounds(20, world, 0))
+        assert(!isInbounds(0, world, 40))
+        assert(!isInbounds(-1, world, 10))
+        assert(!isInbounds(10, world, -1))
+    }
+
+    @Test
+    fun checkNeighboursTest(){
+        val world = Array(3) {Array(3) {false} }
+        world[0][0] = true
+        world[0][1] = true
+        world[1][0] = true
+        assert(checkNeighbours(world, 0, 0) == 2)
+        assert(checkNeighbours(world, 1, 1) == 3)
+    }
+
+    @Test
+    fun nextStateTest(){
+        val world = Array(3) {Array(3) {false} }
+        world[0][0] = true
+        world[0][1] = true
+        world[1][0] = true
+        val nextWorld = nextState(world)
+        assert(nextWorld[0][0])
+        assert(nextWorld[0][1])
+        assert(nextWorld[1][0])
+        assert(nextWorld[1][1])
+    }
 }
